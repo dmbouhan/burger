1324b6bedecc33a11983cebe1e424dc916b5bdf8
delia <deliabouhan10@gmail.com>
2020-01-13 14:57:03 -0600

    added image

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Sets up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
 //make connection
 connection.connect(function(err) {
     if (err) {
@@ -16,6 +19,7 @@ connection.connect(function(err) {
         return;
     }
     console.log("connected as id " + connection.threadId);
+    console.log("live on port 3000");
 });
 
 module.exports = connection;
